3e5ddc648f638148c8ab87e00f75a5e90aafee8b
Chase McLain <chasermclain@gmail.com>
2025-09-23 15:34:48 -0600

    Citadel event model

---
+++ server/src/models/CitadelEvent.js
@@ -0,0 +1,34 @@
+import { Schema } from "mongoose";
+
+
+
+
+export const CitadelEventSchema = new Schema({
+
+  id: { type: Schema.ObjectId, required: true },
+  creatorId: { type: Schema.ObjectId, required: true },
+  name: {type: String, required: true, minlength: 3, maxlength: 50 },
+  description: { type: String, required: true, minlength: 15, maxlength: 1000 },
+  coverImg: { type: String, required: true, minlength: 1, maxlength: 500 },
+  location: { type: String, required: true, minlength: 1, maxlength: 500 },
+  capacity: { type: Number, required: true, minlength: 1, maxlength: 5000 },
+  startDate: {type: Date, required: true },
+  isCanceled: { type: Boolean, required: true, default: false },
+  type: { type: String, enum: ['concert', 'convention', 'sport', 'digital']}
+
+},
+{
+
+  timestamps: true, versionKey: false, toJSON: { virtuals: true }
+
+}
+)
+
+
+
+
+
+
+
+
+
--- server/www/env.js
+++ server/www/env.js
@@ -1,3 +1,7 @@
 export const domain = 'dev-gdmi1jflr5z60pgv.us.auth0.com'
-export const clientId = 'https://Auth.bigratfoundation.com'
-export const audience = '5fFnhGltXZVbQ73tZa24zPc5wQOQ0jDY'
+export const clientId = '5fFnhGltXZVbQ73tZa24zPc5wQOQ0jDY'
+export const audience = 'https://Auth.bigratfoundation.com'
+
+
+
+
