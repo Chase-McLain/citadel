f39f0dedb9252ac8d4f50345f9391e153e999275
Chase McLain <chasermclain@gmail.com>
2025-09-23 15:37:02 -0600

    Citadel events registered

--- server/src/db/DbContext.js
+++ server/src/db/DbContext.js
@@ -1,10 +1,16 @@
 import mongoose from 'mongoose'
 import { AccountSchema } from '../models/Account.js'
 import { ValueSchema } from '../models/Value.js'
+import { CitadelEventSchema } from '../models/CitadelEvent.js';
 
 class DbContext {
+  
   Values = mongoose.model('Value', ValueSchema);
+
   Account = mongoose.model('Account', AccountSchema);
+  
+  CitadelEvents = mongoose.model('CitadelEvent', CitadelEventSchema)
+
 }
 
 export const dbContext = new DbContext()
