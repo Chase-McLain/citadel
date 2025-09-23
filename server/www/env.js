cd2b91b4985be397422c083deddbfc28bbdbf7a9
Chase McLain <chasermclain@gmail.com>
2025-09-23 15:04:58 -0600

    env file setup

--- server/www/env.js
+++ server/www/env.js
@@ -1,3 +1,3 @@
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-gdmi1jflr5z60pgv.us.auth0.com'
+export const clientId = 'https://Auth.bigratfoundation.com'
+export const audience = '5fFnhGltXZVbQ73tZa24zPc5wQOQ0jDY'
