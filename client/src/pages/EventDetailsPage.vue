2b4fc77f8846149b2262429ba41b0ae19d918bb0
Chase McLain <chasermclain@gmail.com>
2025-09-25 16:45:59 -0600

    event details page created

--- client/src/pages/EventDetailsPage.vue
+++ client/src/pages/EventDetailsPage.vue
@@ -4,7 +4,13 @@
 
 
 <template>
-  event
+  <main class="container-fluid">
+    <section class="row">
+      <div class="col">
+        <h5>Event Details</h5>
+      </div>
+    </section>
+  </main>
 </template>
 
 
