863f35b487d3570773aece4852d776ecf81264bc
Chase McLain <chasermclain@gmail.com>
2025-09-25 11:35:46 -0600

    ticket model on client

---
+++ client/src/models/Ticket.js
@@ -0,0 +1,24 @@
+
+
+
+
+export class Ticket{
+  constructor(data){
+
+    this.id = data._id
+    this.accountId = data.accountId
+    this.eventId = data.eventId
+    this.createdAt = data.createdAt
+    this.updatedAt = data.updatedAt
+
+  }
+}
+
+
+
+
+
+
+
+
+
