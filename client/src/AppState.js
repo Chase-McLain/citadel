fb3806ccf4dad71f2f2f75c28eb7f52652365e3b
Chase McLain <chasermclain@gmail.com>
2025-09-25 11:27:37 -0600

    citadel event in appstate

--- client/src/AppState.js
+++ client/src/AppState.js
@@ -1,10 +1,18 @@
 import { reactive } from 'vue'
+import { CitadelEvent } from './models/CitadelEvent.js'
 
 // NOTE AppState is a reactive object to contain app level data
 export const AppState = reactive({
+
   /**@type {import('@bcwdev/auth0provider-client').Identity} */
   identity: null,
+
   /** @type {import('./models/Account.js').Account} user info from the database*/
-  account: null
+  account: null,
+
+  /** @type {import('./models/CitadelEvent.js').CitadelEvent[]} user info from the database*/
+  CitadelEvents: [],
+
+
 })
 
