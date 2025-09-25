6d2569a5e7ace41f7eac7248271bf47b27617376
Chase McLain <chasermclain@gmail.com>
2025-09-24 18:07:05 -0600

    Tickets registered

--- server/src/db/DbContext.js
+++ server/src/db/DbContext.js
@@ -2,14 +2,17 @@ import mongoose from 'mongoose'
 import { AccountSchema } from '../models/Account.js'
 import { ValueSchema } from '../models/Value.js'
 import { CitadelEventSchema } from '../models/CitadelEvent.js';
+import { TicketSchema } from '../models/Ticket.js';
 
 class DbContext {
-  
+
   Values = mongoose.model('Value', ValueSchema);
 
   Account = mongoose.model('Account', AccountSchema);
   
-  CitadelEvents = mongoose.model('CitadelEvent', CitadelEventSchema)
+  CitadelEvents = mongoose.model('CitadelEvent', CitadelEventSchema);
+
+  Tickets = mongoose.model('Ticket', TicketSchema);
 
 }
 
